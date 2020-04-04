237f42283f132b203db1ae49fc1a577c1c1a5124
DmitriyMolodozhonov <dima.sahes@bk.ru>
2020-04-04 13:56:17 +0300

    Исходная версия

---
+++ Полет 2.1/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Полет_2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Coral;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(661, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(2, 31);
+            this.label1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 31);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Начать игру";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Полет";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Полет 2.1/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Полет_2._1
+{
+    public partial class Form1 : Form
+    {
+        System.Drawing.Bitmap sky, plane, gold, skynight, bum, plane1, plane0;
+        Graphics g; //рабочая графическая область
+        int speed = 1; // скорость полета
+        Rectangle rct; //область в которой находится самолет
+        Rectangle rctgold;
+        Rectangle rctplane1;
+       // Rectangle rctplane0;
+        int money = 0;
+
+        private void button3_Click(object sender, EventArgs e) //начать игру
+        {
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            money = -1;
+            button3.Visible = false;
+            speed = 1;
+            rctplane1.X = 680 + rnd.Next(this.ClientSize.Width - 10 - plane1.Width); //определение координат самолета
+            rctplane1.Y = 58 + rnd.Next(this.ClientSize.Height - 90 - plane1.Height); // в рандомном месте
+            label1.Text = money.ToString();
+        }
+
+        Boolean demo = true; // true - самолет скрывается в облаках
+
+
+        System.Random rnd;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            KeyPreview = true; // клавиши
+            KeyDown += (s, e) =>
+            {
+                if (e.KeyValue == (char)Keys.W)
+                {
+                    rct.Y += -10;
+                    g.DrawImage(plane0, rct.X, rct.Y);
+                }
+                else if (e.KeyValue == (char)Keys.S)
+                {
+                    rct.Y += +10;
+                }
+            };
+            try
+            {
+
+                sky = new Bitmap("sky.bmp");     // небо
+                bum = new Bitmap("bum.bmp");     // авария
+                skynight = new Bitmap("night.bmp");     // ночь
+                plane = new Bitmap("plane.bmp"); // самолет
+                gold = new Bitmap("gold.bmp");// монета
+                plane1 = new Bitmap("plane1.bmp"); // самолет 2
+                plane0 = new Bitmap("plane0.bmp");// самолет вверх
+
+                this.BackgroundImage = new Bitmap("sky.bmp");
+
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString(), "Полет", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close(); // закрыть приложение
+                return;
+            }
+            //сделать прозрачный фон во круг объектов
+            plane.MakeTransparent();
+            gold.MakeTransparent();
+            plane1.MakeTransparent();
+            plane0.MakeTransparent();
+            //задать размер формы в зависимости от картинки
+            this.ClientSize = new System.Drawing.Size(new Point(BackgroundImage.Width, BackgroundImage.Height));
+            //задать вид границы окна
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            g = Graphics.FromImage(BackgroundImage);
+            rnd = new System.Random();
+            //-----------------------------------
+            rct.X = -40;
+            rct.Y = 170 + rnd.Next(20);
+            //-----------------------------------
+            rctplane1.X = 680;
+            rctplane1.Y = 150 + rnd.Next(30);
+            //-----------------------------------
+            rctgold.X = -40;
+            rctgold.Y = 170 + rnd.Next(20);
+            //-----------------------------------
+            rct.Width = plane.Width;
+            rct.Height = plane.Height;
+            //-----------------------------------
+            rctgold.Width = gold.Width;
+            rctgold.Height = gold.Height;
+            //-----------------------------------
+            rctplane1.Width = plane1.Width;
+            rctplane1.Height = plane1.Height;
+            //-----------------------------------
+            timer2.Interval = 20;
+            timer2.Enabled = true;
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            g.DrawImage(sky, new Point(0, 0));
+
+            if (rct.X < this.ClientRectangle.Width)
+            {
+                rct.X += speed;
+            }
+
+            else
+            {
+                rct.X = -40;
+                rct.Y = 20 + rnd.Next(this.ClientSize.Height - 40 - plane.Height);
+            }
+            if (rctplane1.X < this.ClientRectangle.Width)
+            {
+                rctplane1.X -= speed;
+            }
+
+            else
+            {
+                rctplane1.X = 680;
+                rctplane1.Y = 60 + rnd.Next(this.ClientSize.Width - 20 - plane1.Height);
+            }
+            //----------------столкновение--------------------------------------------
+            if (rct.IntersectsWith(rctgold)) //столкновение с монетой
+            {
+                money++;
+                speed++;
+                label1.Text = money.ToString();
+                    rctgold.X = 30 + rnd.Next(this.ClientSize.Width - 10 - gold.Width); //определение координат монеты
+                    rctgold.Y = 58 + rnd.Next(this.ClientSize.Height - 90 - gold.Height); // в рандомном месте
+                rctplane1.X = 680 + rnd.Next(this.ClientSize.Width - 10 - plane1.Width); //определение координат самолета
+                rctplane1.Y = 58 + rnd.Next(this.ClientSize.Height - 90 - plane1.Height); // в рандомном месте
+            }
+            if (rct.IntersectsWith(rctplane1))//столкновение с самолетом
+            {
+                this.timer2.Tick -= new System.EventHandler(this.timer2_Tick);
+                g.DrawImage(bum, new Point(0, 0));
+                button3.Visible = true;
+            }
+            //----------------------------------------------------------------------------
+            if ((money > 10)) //смена фона
+            {
+                g.DrawImage(skynight, new Point(0, 0));
+            }
+            //рисовка объекта
+            g.DrawImage(plane, rct.X, rct.Y);
+            g.DrawImage(plane1, rctplane1.X, rctplane1.Y);
+            g.DrawImage(gold, rctgold.X, rctgold.Y);
+            if (!demo) //обновить область формы в которой находится рисунок
+            {
+                this.Invalidate(rct);
+                this.Invalidate(rctplane1);
+            }
+            else
+            {
+                Rectangle reg = new Rectangle(0, 0, sky.Width - 0, sky.Height - 0);
+                //  g.DrawRectangle(Pens.Black, reg.X, reg.Y, reg.Width - 1, reg.Height - 1);
+
+                this.Invalidate(reg); // обновить область
+            }
+
+        }
+    }
+}
